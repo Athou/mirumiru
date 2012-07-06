cc582e9fd7bfa572dbdc4e233d502dada534bc5f
Athou <jeremiepanzer@gmail.com>
2012-07-06 09:24:12 +0200

    db populate via bean for sequence management

--- src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-insert into User (id, name) values (1, 'Admin')
-
-insert into News (id, title, content, author_id, created) values (1, 'News 1', 'Hello, world', 1, '2010-01-01')
-insert into News (id, title, content, author_id, created) values (2, 'News 2', 'Hello, world', 1, '2010-01-02')
-insert into News (id, title, content, author_id, created) values (3, 'News 3', 'Hello, world', 1, '2010-01-03')
-insert into News (id, title, content, author_id, created) values (4, 'News 4', 'Hello, world', 1, '2010-01-04')
-insert into News (id, title, content, author_id, created) values (5, 'News 5', 'Hello, world', 1, '2010-01-05')
