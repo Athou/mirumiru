b02b2bc4007071fd9e6dc2e1d4d40854a7e1a0c6
Athou <jeremiepanzer@gmail.com>
2012-07-17 15:03:58 +0200

    faqpage scaffolding

--- src/main/java/fr/mirumiru/pages/FAQPage.java
+++ src/main/java/fr/mirumiru/pages/FAQPage.java
@@ -1,16 +1,68 @@
 package fr.mirumiru.pages;
 
+import java.util.List;
+import java.util.MissingResourceException;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+
+import com.google.common.collect.Lists;
+
+@SuppressWarnings("serial")
 public class FAQPage extends ContentPage {
 
-	private static final long serialVersionUID = 2354826787598549057L;
-
 	public FAQPage() {
+
+		IModel<List<QuestionAndAnswer>> model = new LoadableDetachableModel<List<QuestionAndAnswer>>() {
+			@Override
+			protected List<QuestionAndAnswer> load() {
+				List<QuestionAndAnswer> list = Lists.newArrayList();
+
+				String questionProp = "faq.question";
+				String answerProp = "faq.answer";
+				int i = 1;
+
+				try {
+					while (true) {
+						QuestionAndAnswer qaa = new QuestionAndAnswer();
+						qaa.question = getLocalizedString(questionProp + i);
+						qaa.answer = getLocalizedString(answerProp + i);
+						list.add(qaa);
+						i++;
+					}
+				} catch (MissingResourceException e) {
+					// do nothing
+				}
+
+				return list;
+			}
+
+		};
+
+		ListView<QuestionAndAnswer> view = new ListView<QuestionAndAnswer>(
+				"faqlist", model) {
+			@Override
+			protected void populateItem(ListItem<QuestionAndAnswer> item) {
+				QuestionAndAnswer qaa = item.getModelObject();
+				item.add(new Label("question", qaa.question));
+				item.add(new Label("answer", qaa.answer));
+			}
+		};
+		add(view);
 
 	}
 
 	@Override
 	protected String getTitle() {
 		return "faq";
+	}
+
+	private class QuestionAndAnswer {
+		public String question;
+		public String answer;
 	}
 
 }
