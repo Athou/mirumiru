ee24f660a0200647bad9f2709d4a97a3e66f18c4
Athou <jeremiepanzer@gmail.com>
2012-07-18 10:23:53 +0200

    fail gracefully when unable to init facebook infos

--- src/main/java/fr/mirumiru/services/FacebookService.java
+++ src/main/java/fr/mirumiru/services/FacebookService.java
@@ -43,25 +43,34 @@ public class FacebookService {
 	@PostConstruct
 	private void refresh() {
 		synchronized (this) {
-			log.info("Refreshing album list from Facebook");
 
-			FacebookClient client = new DefaultFacebookClient(
-					bundle.getFacebookAuthToken());
+			cache = new FacebookCache(new ArrayList<Album>(),
+					new ArrayList<Post>());
+			try {
+				log.info("Refreshing album list from Facebook");
 
-			Connection<Album> albumConnection = client.fetchConnection(
-					"127903530580737/albums", Album.class);
-			List<Album> albums = new ArrayList<Album>(albumConnection.getData());
-			Collections.sort(albums, new Comparator<Album>() {
-				@Override
-				public int compare(Album o1, Album o2) {
-					return o2.getCreatedTime().compareTo(o1.getCreatedTime());
-				}
-			});
+				FacebookClient client = new DefaultFacebookClient(
+						bundle.getFacebookAuthToken());
 
-			Connection<Post> postConnection = client.fetchConnection(
-					"127903530580737/statuses&limit=1000", Post.class);
-			List<Post> posts = new ArrayList<Post>(postConnection.getData());
-			cache = new FacebookCache(albums, posts);
+				Connection<Album> albumConnection = client.fetchConnection(
+						"127903530580737/albums", Album.class);
+				List<Album> albums = new ArrayList<Album>(
+						albumConnection.getData());
+				Collections.sort(albums, new Comparator<Album>() {
+					@Override
+					public int compare(Album o1, Album o2) {
+						return o2.getCreatedTime().compareTo(
+								o1.getCreatedTime());
+					}
+				});
+
+				Connection<Post> postConnection = client.fetchConnection(
+						"127903530580737/statuses&limit=1000", Post.class);
+				List<Post> posts = new ArrayList<Post>(postConnection.getData());
+				cache = new FacebookCache(albums, posts);
+			} catch (Exception e) {
+				log.fatal("Could not refresh facebook infos", e);
+			}
 		}
 	}
 
