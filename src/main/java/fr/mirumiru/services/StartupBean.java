7b71552597463c92a72c1b05df49c2b5c94074da
Athou <jeremiepanzer@gmail.com>
2012-08-29 15:53:53 +0200

    disable restfb logging

--- src/main/java/fr/mirumiru/services/StartupBean.java
+++ src/main/java/fr/mirumiru/services/StartupBean.java
@@ -1,10 +1,16 @@
 package fr.mirumiru.services;
 
 import java.util.Calendar;
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.DefaultWebRequestor;
 
 @Startup
 @Singleton
@@ -15,6 +21,17 @@ public class StartupBean {
 	@PostConstruct
 	public void init() {
 		startupTime = Calendar.getInstance();
+
+		final Filter disableLog = new Filter() {
+			@Override
+			public boolean isLoggable(LogRecord record) {
+				return false;
+			}
+		};
+		Logger.getLogger(DefaultFacebookClient.class.getName()).setFilter(
+				disableLog);
+		Logger.getLogger(DefaultWebRequestor.class.getName()).setFilter(
+				disableLog);
 	}
 
 	public Calendar getStartupTime() {
