5d8f8f6bb850cd1c76727835181e2c63ddcf3a5e
Athou <jeremiepanzer@gmail.com>
2012-08-31 15:03:20 +0200

    removed useless annotation

--- src/main/java/fr/mirumiru/services/UserDAO.java
+++ src/main/java/fr/mirumiru/services/UserDAO.java
@@ -1,11 +1,9 @@
 package fr.mirumiru.services;
 
-import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
 import fr.mirumiru.model.User;
 
-@LocalBean
 @Stateless
 public class UserDAO extends GenericDAO<User> {
 
