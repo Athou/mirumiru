3422e72317b2817107a535cfe555725e7c25f586
Athou <jeremiepanzer@gmail.com>
2012-11-01 11:47:24 +0100

    separator tweak

--- src/main/java/fr/mirumiru/pages/FacebookNewsPage.java
+++ src/main/java/fr/mirumiru/pages/FacebookNewsPage.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -97,9 +96,4 @@ public class FacebookNewsPage extends ContentPage {
 		return "news";
 	}
 
-	@Override
-	public void renderHead(IHeaderResponse response) {
-		super.renderHead(response);
-		response.render(WicketUtils.loadCSS(FacebookNewsPage.class));
-	}
 }
