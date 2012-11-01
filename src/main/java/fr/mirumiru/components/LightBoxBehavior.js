af4742ab305e1d2299250796863b57aa137dabf9
Athou <jeremiepanzer@gmail.com>
2012-11-01 10:33:16 +0100

    add missing image path

--- src/main/java/fr/mirumiru/components/LightBoxBehavior.js
+++ src/main/java/fr/mirumiru/components/LightBoxBehavior.js
@@ -3,6 +3,7 @@ $(function() {
 		imageLoading: '../images/lightbox/lightbox-ico-loading.gif',
 		imageBtnClose: '../images/lightbox/lightbox-btn-close.gif',
 		imageBtnPrev: '../images/lightbox/lightbox-btn-prev.gif',
-		imageBtnNext: '../images/lightbox/lightbox-btn-next.gif'
+		imageBtnNext: '../images/lightbox/lightbox-btn-next.gif',
+		imageBlank:  '../images/lightbox/lightbox-blank.gif'
 	   });
 });
